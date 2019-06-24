68adb3e57f6a1adbca567958b799d211f1e7266e
phucAone <phucaone@gmail.com>
2019-06-24 12:01:16 -0700

    aa

---
+++ gggg/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gggg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
